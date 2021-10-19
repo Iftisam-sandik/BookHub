98d9a6f8f99095085aaf1e2a138e93b840dab4ed
Iftisam-sandik <sandik55575@gmail.com>
2021-10-20 00:03:18 +0600

    Inital Commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookHub.Models;
+
+namespace BookHub.Controllers
+{
+    public class BookController : Controller
+    {
+        BookHubEntities db = new BookHubEntities();
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Book()
+        {
+            var list = db.BookDetails;
+            return View(list);
+        }
+
+        public ActionResult ThrillerBooks()
+        {
+            string category = "Thriller";
+            var list = from r in db.BookDetails
+                       where r.BookCategory == category
+                       select r;
+            return View(list);
+        }
+
+        public ActionResult DetectiveBooks()
+        {
+            string category = "Detective";
+            var list = from r in db.BookDetails
+                       where r.BookCategory == category
+                       select r;
+            return View(list);
+        }
+
+        public ActionResult ScienceFictionBooks()
+        {
+            string category = "Science Fiction";
+            var list = from r in db.BookDetails
+                       where r.BookCategory == category
+                       select r;
+            return View(list);
+        }
+
+        public ActionResult NovelBooks()
+        {
+            string category = "Novel";
+            var list = from r in db.BookDetails
+                       where r.BookCategory == category
+                       select r;
+            return View(list);
+        }
+
+        public ActionResult FreshArrival()
+        {
+            string category = "Fresh Arrival";
+            var list = from r in db.BookDetails
+                       where r.BookCategory == category
+                       select r;
+            return View(list);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookHub.Models;
+
+namespace BookHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookHubEntities db = new BookHubEntities();
+        public ActionResult Index()
+        {
+            var book = db.BookDetails.Where(x => x.BookName.StartsWith("Star Wars"));
+            return View(book);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult OurTeam()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SearchResult(string searching)
+        {
+            if (!String.IsNullOrEmpty(searching))
+            {
+                var list = db.BookDetails.Where(x => x.BookName.StartsWith(searching)).ToList();
+                if (list != null)
+                {
+                    return View(list);
+                }
+                else
+                {
+                    var list2 = db.BookDetails.Where(x => x.AuthorName.StartsWith(searching)).ToList();
+                    return View(list2);
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+            
+            
+        }
+
+        [HttpPost]
+        public ActionResult ContactUs(ContactU c)
+        {
+            ContactU u = new ContactU();
+
+            u.UserName = c.UserName;
+            u.UserEmail = c.UserEmail;
+            u.Subject = c.Subject;
+            u.Message = c.Message;
+
+            db.ContactUs.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
